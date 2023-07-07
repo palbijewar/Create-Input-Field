e1079cc7fb23b14474a0e2bf083602bba8026eeb
Pal Bijewar <pal.bijewar@gmail.com>
2023-07-07 23:50:51 +0530

    input app

---
+++ READEME.md
@@ -0,0 +1,6 @@
+Assignment- 3:-
+
+Create input field
+When you type a number on that input field immediately generate table for that number and show it on the page
+
+Suppose you typed five on input field, and without any button, click immediately generate a table for five and show it on a table and if you change the number, it should immediately change the table
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css'
+
+function App() {
+const [number, setNumber] = useState(null);
+const [table, setTable] = useState([]);
+
+const handleChange = (e) => {
+  setNumber(e.target.value);
+  if (e.target.value !== '') {
+    setTable(generateTable());
+  } else {
+    setTable([]);
+  }
+};
+
+const generateTable = () => {
+  var table = [];
+  for (let i = 1; i <= 10; i++) {
+    table.push(
+      <tr key={i}>
+         <td>{`${number} x ${i}`}</td>
+          <td>=</td>
+          <td>{number * i}</td>
+      </tr>
+    )
+  }
+  return table;
+}
+
+  return (
+    <div className='container'>
+      <div className='box'>
+      <h1>Wanna learn table?</h1>
+      <h3>Write the number you want the table of!</h3>
+      <input type="number" value={number} onChange={handleChange} />
+      </div>
+      {number  && (
+        <div className='table-div'>
+      <table value={table}>
+        <tbody>{generateTable()}</tbody>
+      </table>
+      </div>
+      )}
+      
+      </div>
+  );
+}
+
+export default App;
